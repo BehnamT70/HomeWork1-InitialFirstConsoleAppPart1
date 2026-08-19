2e83b140f5fe9120bca925e40bde8d6890be8f7c
Behnam Talebi <behnam.talebi70@gmail.com>
2026-08-19 18:57:59 +0330

    Doing Exercises HomeWore1

---
+++ HomeWork1-InitialFirstConsoleAppPart1/HomeWork1/Fourth_Program/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Fourth_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "CSharp";
+            string userGuess = "";
+            int attempts = 0;
+
+            while (userGuess.ToLower() != secretWord.ToLower())
+            {
+                Console.Write("Guess the secret word: ");
+                userGuess = Console.ReadLine();
+
+                attempts++;
+
+                if (userGuess.ToLower() == secretWord.ToLower())
+                {
+                    Console.WriteLine("Congratulations! You guessed the word correctly.");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong guess. Try again.");
+                }
+            }
+
+            Console.WriteLine($"Total attempts: {attempts}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork1-InitialFirstConsoleAppPart1/HomeWork1/ThirdProgram/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ThirdProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int secretNumber = random.Next(1, 11);
+
+            bool guessedCorrectly = false;
+
+            while (!guessedCorrectly)
+            {
+                for (int attempt = 1; attempt <= 5; attempt++)
+                {
+                    Console.Write($"Attempt {attempt}/5 - Guess a Number Between 1 and 10: ");
+                    string input = Console.ReadLine();
+
+                    if (int.TryParse(input, out int guess) && guess >= 1 && guess <= 10)
+                    {
+                        if (guess == secretNumber)
+                        {
+                            Console.WriteLine("Congratulations! You Guessed The Correct Number!");
+                            guessedCorrectly = true;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong Guess!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please Enter a Number Between 1 and 10.");
+                    }
+                }
+
+                if (!guessedCorrectly)
+                {
+                    Console.WriteLine($"Sorry! You Did Not Guess The Number In 5 Attempts.");
+                    Console.WriteLine($"The Correct Number Was {secretNumber}.");
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
